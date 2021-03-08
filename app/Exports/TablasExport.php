5b40e77dde8032eb6d36434f9d0adab510cde34e
Alfredo Galvan <agalvan@clustermx.com>
2021-03-08 15:11:11 -0600

    export

--- app/Exports/TablasExport.php
+++ app/Exports/TablasExport.php
@@ -10,8 +10,23 @@ class TablasExport implements FromCollection
     /**
     * @return \Illuminate\Support\Collection
     */
+
+    public $token;
+
+
+    public function __construct($token)
+    {
+
+        $this->token = $token;
+    }
+
     public function collection()
     {
-        return Tabla::all();
+        return Tabla::where('token', $this->token)->get();
+    }
+
+    public function headings()
+    {
+        return ["Fecha de pago", "Saldo Inicial", "Pago", "Capital", "Intereses", "Saldo Inicial"];
     }
 }
